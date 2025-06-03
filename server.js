8d59716a1f540341cc0f199de760a1ed25f4100a
AbnerGZL <abner.gomez@tecsup.edu.pe>
2025-06-03 13:01:59 -0500

    modificacion de cors

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const corsOptions = {
     origin: 'http://localhost:8080',
     origin: 'http://localhost:3000',
+    origin: 'https://frontexamen3.onrender.com/',
     credentials: true
 };
 
